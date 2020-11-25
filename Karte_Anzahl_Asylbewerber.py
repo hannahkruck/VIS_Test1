b384de0f887f6ead553d0a2974f983e5cf00e7cb
Hannah Kruck <Hannah.Kruck@Student.Reutlingen-University.de>
2020-11-25 21:37:41 +0100

    Added Details Page and adjustments in Main Page (Map size, Filter)

--- Karte_Anzahl_Asylbewerber.py
+++ Karte_Anzahl_Asylbewerber.py
@@ -12,7 +12,7 @@ import numpy as np
 # In[2]:
 #Datentabelle laden und ausblenden
 #def load_data():
-    
+  
 # read CSV
 df = pd.read_csv("https://raw.githubusercontent.com/hannahkruck/VIS_Test1/Develop/map.csv", encoding ="utf8", sep=";")
 
@@ -75,56 +75,59 @@ fig.update_layout(
         showframe=False,
         showcoastlines=False,
         projection_type='equirectangular'
-    )
+    ),
+    autosize=True,
+    #width=1500,
+    #height=1080,
 )
 #--------------------Slider Steps------------------------------------#
 #https://stackoverflow.com/questions/46777047/how-to-make-a-choropleth-map-with-a-slider-using-plotly
 
 
+st.plotly_chart(fig, width=1920)
 
+st.slider("Timeline Years", 2010,  2019)
 
-st.plotly_chart(fig)
+#Sidebar
+st.sidebar.header("Filters")
+st.sidebar.multiselect("Select Age", ("All", "under 18", "18 - 35", "26 - 35", "36 - 45"))
+st.sidebar.multiselect("Select Gender", ("All", "Male", "Female", "Other/unknown"))
+st.sidebar.multiselect("Select Origin Country", ("All", "Belgium", "Bulgaria", "Czech Republic", "Denmark", "Germany", "Estonia", "Ireland", "Greece", "Spain"))
+st.sidebar.multiselect("Select Destination Country", ("All", "Belgium", "Bulgaria", "Czech Republic", "Denmark", "Germany", "Estonia", "Ireland", "Greece", "Spain"))
 
-
-
-
-st.sidebar.header("Filteroptionen")
-st.sidebar.multiselect("Select Age", ("Select all", "under 18", "18 - 35", "26 - 35", "36 - 45"))
-
+#st.sidebar.subheader("Filter option 2")
 # Checkbox widget
 #checkbox = st.sidebar.checkbox("Datensatz einblenden")
 #print(checkbox)
-
-st.sidebar.subheader("Filter option 2")
 #add select widget
 #select_box_age = st.sidebar.selectbox(label='AGE', options=columns_checkbox)
 #print(select_box_age)
 
 #https://stackoverflow.com/questions/63158617/multiple-same-key-button-generated-in-streamlit
-st.sidebar.text('AGE')
+#st.sidebar.text('AGE')
 #select_box1 = st.sidebar.checkbox("Select all", "under 18", "18 - 35", "26 - 35", "36 - 45")
-select_box1 = st.sidebar.checkbox(label="Select all", key="1")
-select_box2 = st.sidebar.checkbox(label="under 18", key="2")
-select_box3 = st.sidebar.checkbox(label="18 - 35", key="3")
-select_box4 = st.sidebar.checkbox(label="26 - 35", key="4")
-select_box5 = st.sidebar.checkbox(label="36 - 45", key="5")
+#select_box1 = st.sidebar.checkbox(label="Select all", key="1")
+#select_box2 = st.sidebar.checkbox(label="under 18", key="2")
+#select_box3 = st.sidebar.checkbox(label="18 - 35", key="3")
+#select_box4 = st.sidebar.checkbox(label="26 - 35", key="4")
+#select_box5 = st.sidebar.checkbox(label="36 - 45", key="5")
 
-print(select_box1)
-print(select_box2)
-print(select_box3)
-print(select_box4)
-print(select_box5)
+#print(select_box1)
+#print(select_box2)
+#print(select_box3)
+#print(select_box4)
+#print(select_box5)
 
 
-st.sidebar.text('GENDER')
-select_box6 = st.sidebar.checkbox(label="Select all", key="6")
-select_box7 = st.sidebar.checkbox(label="Female", key="7")
-select_box8 = st.sidebar.checkbox(label="Male", key="8")
-select_box9 = st.sidebar.checkbox(label="Other/unknown", key="9")
-print(select_box6)
-print(select_box7)
-print(select_box8)
-print(select_box9)
+#st.sidebar.text('GENDER')
+#select_box6 = st.sidebar.checkbox(label="Select all", key="6")
+#select_box7 = st.sidebar.checkbox(label="Female", key="7")
+#select_box8 = st.sidebar.checkbox(label="Male", key="8")
+#select_box9 = st.sidebar.checkbox(label="Other/unknown", key="9")
+#print(select_box6)
+#print(select_box7)
+#print(select_box8)
+#print(select_box9)
 
 # In[ ]:
 
