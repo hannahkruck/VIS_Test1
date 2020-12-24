2a2c0f59e821f4b61d0263c0d1bd1d52dbfb60c3
YaildaAini <yailda.aini@student.reutlingen-university.de>
2020-12-24 02:59:46 +0100

    Update welcome.py
    Input to Welcome Site - reihenfolge und einige aktualisierungen folgen nocht

--- src/pages/welcome.py
+++ src/pages/welcome.py
@@ -19,15 +19,25 @@ def write():
 # CSS Startseite
 	st.markdown("""
 	<style>
-	body {
-		color: black;
-		font-color: black;
-		background-color: white;
-	}
-
-	h3 {
-		color: #1878a1;
-	}
+		body {
+			color: black;
+			font-color: black;
+			background-color: white;
+		}
+		h3 {
+			text-align: justify;
+		}
+		p {
+			text-align: justify; 
+		}
+		#text {
+			color: #1878a1;
+			font-weight:normal;
+		}
+		#expand_three {
+			color: #1878a1;
+			font-size: 18px;
+		}
 	</style>
     """, unsafe_allow_html=True)
 
@@ -35,74 +45,169 @@ def write():
 
 # HTML Inhalt
 	html = """
-	<h3>"Toolname" ist ein Visualisierungstool ..." <br>
-	Ziel<br>
-	Was kann Tool alles zeigen<br>
-	Welche Optionen haben Nutzer</h3>
-	
+		<h3 id=text><b>Visuasyl</b> is an interactive visualisation tool that visually illustrates information about asylum applications and the development in Europe. 
+			The aim of visuasyl is to provide users with an insight and overview of the number of annual asylum applications and migration in Europe 
+			between 2010 and 2019, using different visualisation methods. 
+			<br>
+			Visuasyl was mainly developed for political parties, politicians and non-governmental organisations (NGOs). 
+			Because of the simple functionalities, visuasyl can also be used by non-experts and interested parties.	
+			<br>
+		</h3>
 	<hr>
-	<br>
 	"""
 	st.markdown(html, unsafe_allow_html=True)
 	
 	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
-	my_expander_one = st.beta_expander("Where does the data come from?", expanded=False)
+	my_expander_three = st.beta_expander("What informations and interaction options offers visuasyl?", expanded=False)
+	with my_expander_three:
+		html_three = """ 
+			<b id=expand_three>Informations</b><br>
+			<u>Chorpleth Map</u>:<br>
+			The number of asylum applications per country in Europe and the number of refugees per country worldwide for the selected year.
+
+			<u>Line Map</u>:<br>
+			Route of the refugees: From which countries the asylum seekers originate in relation to a specific country and
+			where the asylum seekers are fleeing from in relation to a particular country of origin.
+			
+			<u>Pie Chart</u>:<br>
+			Represents the age distribution worldwide for the selected year. 
+			
+			<u>Sankey Diagram</u>:<br>
+			Shows the distribution of asylum applications from the different countries of origin to the different countries of destination. 
+			Also the Top 10 destination countries of a year are illustrated here. 
+
+			<u>Time graph</u>:<br>
+			Shows the total number of asylum applications over the years.			
+			
+			<br><b id=expand_three>Interaction Options:</b><br>
+			<u>Navigation Sidebar</u>:<br>
+			Allows to switch between welcome, home and detail site.<br>
+			Also the navigation sidebar contains filter options for the maps:
+			- Switching between the choropleth and line map 
+			- Filtering the map by selecting Age, Gender
+			- Special filters for the Choropleth Map: Application to target country, Application by country of origin
+			- Special filters for the  Line Map: Select a Type for Map (Target or Origin Country), Select a specific Country
+
+			By adjusting the time slider, users can update the data of the charts to the selected year.
+
+
+			
+			"""
+
+
+		st.markdown(html_three, unsafe_allow_html=True)
+
+
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_one = st.beta_expander("Where does the source data come from?", expanded=False)
 	
 	with my_expander_one:
 		html_one = """ 
-			<p> Die Daten stammen aus der ... </p>
-			Datensatz: <a target="_blank" href="https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=migr_asyappctza&lang=de">Asylbewerber und erstmalige Asylbewerber nach Staatsangehörigkeit, Alter und Geschlecht - jährliche aggregierte Daten (gerundet)<br></a>
-			<br>"""
+
+			<p>The dataset used to create the visualisation tool was provided by Eurostat, the statistical office of the European Union.
+			Eurostat is an administrative unit of the European Union with the mission to produce official European statistics.
+			<br>These data are provided by the national ministries of the interior and related official institutions.  
+			<br>Link to the Dataset: <a target="_blank" href="https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=migr_asyappctza&lang=de">Asylum and first time asylum applicants by citizenship, age and sex - annual aggregated data (rounded)<br></a>
+			<br>
+			</p>"""
 		st.markdown(html_one, unsafe_allow_html=True)
 
 
 	
 	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
 	my_expander_two = st.beta_expander("Implementation of the visualisation tool", expanded=False)
-
+	
 	with my_expander_two:
 		html_two = """ 
 			<p> The visualisation tool was implemented with the open source framework <a target="_blank" href="https://www.streamlit.io"><b> Streamlit</b></a>.<br> 
 			Streamlit is both a library and a framework for Python. 
 			It allows users to create and publish interactive web apps with a graphical user interface and data visualisation. 
-			<br><br>The advantage of Streamlit is that no front-end experience is required and we can create interactive graphical user interfaces and visualisations using only the Python programming language. Probably the strongest point in choosing Streamlit is the time aspect. Because with Streamlit we can invest more time in processing and visualising the data than in dealing with the front end. <br>
+			<br><br>The advantage of Streamlit is that no front-end experience is required and we can create interactive graphical user interfaces and visualisations using only the Python programming language. 
+			Probably the strongest point in choosing Streamlit is the time aspect. Because with Streamlit we can invest more time in processing and visualising the data than in dealing with the front end. <br>
 			<br>Furthermore, it is possible to integrate various libraries in Streamlit in order to create diagrams.
 			For the visualisation of our diagrams and filter options, libraries such as Streamlit, Plotly and Pandas were used:
-			<br>
+			<br><br>
+			<u>Pandas</u>
 				<ul>
-					<li>Choropleth and Line Maps</li>
+					<li>read CSV files</li>
 				</ul>
-			</p>"""
+			<u>Plotly (plotly.graph_objects)</u>
+				<ul>
+					<li>To create the maps, pie chart, sankey diagram and time graph</li>
+				</ul>
+			<u>Streamlit</u>
+				<ul>
+					<li>Create navigation sidebar, sliders, parameter filter and structure of the site</li>
+				</ul>
+			<u>HTML and CSS</u>
+				<ul>
+					<li>For user-specific design of the website and the information button</li>
+				</ul>
+
+
+		</p>"""
 			
 		st.markdown(html_two, unsafe_allow_html=True)
 
-
 	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
-	my_expander_three = st.beta_expander("Expand3", expanded=False)
+	my_expander_three = st.beta_expander("Expand 3", expanded=False)
 	with my_expander_three:
 		html_three = """ 
-			<p> Expand3</p>"""
+			<p> Inhalt </p>"""
 		st.markdown(html_three, unsafe_allow_html=True)
 
 	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
-	my_expander_four = st.beta_expander("Expander 4", expanded=False)
+	my_expander_four = st.beta_expander("Insights through the visualisation tool", expanded=False)
 	with my_expander_four:
 		html_four = """ 
 			<p> Inhalt </p>"""
 		st.markdown(html_four, unsafe_allow_html=True)
 
+
 	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
 	my_expander_five = st.beta_expander("About us", expanded=False)
 	with my_expander_five:
 		html_five = """ 
-			<p> Inhalt </p>"""
+			<p>The visualisation tool <b>visuasyl</b> was developed as part of the module "Visualisation". 
+			The aim of this module has been to find a dataset of interest and to develop a visualisation tool. 
+			<br>Visuasyl was developed by five students (3rd semester) of the Human-Centered Computing programme at Reutlingen University:
+			<br>
+			<ul>
+				<li> Rahel Anna, Illi<br></li>
+				<li> Jessica, Giebel<br></li>
+				<li> Yailda, Aini<br></li>
+				<li> Hannah, Kruck<br></li>
+				<li> Manuel, Haynes<br></li>
+			</ul>
+
+			<u>Why did we choose this theme and data set?</u><br>
+			The issue of refugees and migration has always been an important topic and affects all citizens, whether directly or only indirectly. According to the German Federal Government, more than 70 million people are on the brink of flight worldwide today [1]. 
+			Wars, persecution or the financial situation lead people to leave their homeland in order to find a safer and better life.
+			We chose this topic and data set to provide information on migration and refugees to interested parties with the help of a visualisation tool.
+
+			<u>What is the purpose of data visualization?</u><br>
+			To gain insights from data, it is not enough to look at the raw data. 
+			It is usually unstructured and not very useful. It is necessary to analyse the data further so that a conclusion can be drawn from the data. 
+			To make this possible from the data set of asylum applications, we have developed a visualisation tool that allows the data to be illustrated with the help of various diagrams. 
+			The visualisation of the data makes it possible for the user to recognise patterns or correlations within the data sets.
+			
+			</p>"""
 		st.markdown(html_five, unsafe_allow_html=True)
 		
 	
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_four = st.beta_expander("Source", expanded=False)
+	with my_expander_four:
+		html_four = """ 
+			<p>[1] 16.10.2020. Was tut die Bundesregierung im Bereich Migration und Integration?. URL: https://www.bundesregierung.de/breg-de/themen/migration-und-integration</p>"""
+		st.markdown(html_four, unsafe_allow_html=True)
+
+
 	c1, c2 = st.beta_columns((1,1))
 	container = st.beta_container()
 	st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 			
+				
 				
 	
 
